6b22861825fc739e8b8c7314b1a6b6fd78a0e2a7
brikhotso <brenda.rikhotso@gmail.com>
2023-12-13 07:14:08 -0800

    modify func3

--- func3.c
+++ func3.c
@@ -7,24 +7,24 @@
  */
 void pchar(stack_t **stack, unsigned int line_number)
 {
-	int ascii_value;
-
-	if (*stack == NULL)
+	if (*stack != NULL)
+	{
+		if ((*stack)->n >= 0 && (*stack)->n <= 127)
+		{
+			fprintf(stdout, "%c\n",((*stack)->n));
+		}
+		else
+		{
+			fprintf(stderr, "L%u: can't pchar, value out of range\n"
+				, line_number);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
 	{
 		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-
-	ascii_value = (*stack)->n;
-
-	if (ascii_value < 0 || ascii_value > 127)
-	{
-		fprintf(stderr, "L%u: can't pchar, value out of range\n",
-			line_number);
-		exit(EXIT_FAILURE);
-	}
-
-	fprintf(stdout, "%c\n", ascii_value);
 }
 
 /**
