401e0999de581649ecacf8fedd830d0b1f7333e1
brikhotso <brenda.rikhotso@gmail.com>
2023-12-13 07:29:35 -0800

    modify func3

--- func3.c
+++ func3.c
@@ -37,9 +37,11 @@ void pstr(stack_t **stack, unsigned int line_number)
 	stack_t *current = *stack;
 	(void)line_number; /* Unused variable */
 
-	while (current != NULL && current->n != 0 &&
-	   (current->n >= 0 && current->n <= 127))
+	while (current != NULL)
 	{
+		if (current->n > 127 || current->n <= 0)
+			break;
+
 		fprintf(stdout, "%c", current->n);
 		current = current->next;
 	}
