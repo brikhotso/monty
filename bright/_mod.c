07037a0f0771a271fb4e05f789330fed3d9a8a24
Bright <bright.sablah@outlook.com>
2023-12-15 01:44:35 +0000

    Added _mod.c function

---
+++ bright/_mod.c
@@ -0,0 +1,33 @@
+#include "monty.h"
+/**
+ * _mod - mod top of stack y second top stack
+ * @stack: pointer to lists for monty stack
+ * @line_number: number of line opcode occurs on
+ */
+void _mod(stack_t **stack, unsigned int line_number)
+{
+	int mod = 0;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't mod, stack too short\n", line_number);
+		free(var_global.buffer);
+		fclose(var_global.file);
+		free_dlistint(*stack);
+		exit(EXIT_FAILURE);
+	}
+	else if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		free(var_global.buffer);
+		fclose(var_global.file);
+		free_dlistint(*stack);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		mod = (*stack)->n;
+		_pop(stack, line_number);
+		(*stack)->n %= mod;
+	}
+}
