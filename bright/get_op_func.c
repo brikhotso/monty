b5337a4041f765d65a0e203db0dad2ca50f2e976
Bright <bright.sablah@outlook.com>
2023-12-15 01:44:36 +0000

    Added get_op_func.c function

---
+++ bright/get_op_func.c
@@ -0,0 +1,37 @@
+#include "monty.h"
+/**
+ * get_op_func -  checks opcode and returns the correct function
+ * @str: the opcode
+ * Return: returns a functions, or NULL on failure
+ */
+instruct_func get_op_func(char *str)
+{
+	int i;
+
+	instruction_t instruct[] = {
+		{"push", _push},
+		{"pall", _pall},
+		{"pint", _pint},
+		{"pop", _pop},
+		{"swap", _swap},
+		{"add", _add},
+		{"nop", _nop},
+		{"sub", _sub},
+		{"mul", _mul},
+		{"div", _div},
+		{"mod", _mod},
+		{"pchar", _pchar},
+		{"pstr", _pstr},
+		{"rotl", _rotl},
+		{"rotr", _rotr},
+		{NULL, NULL},
+	};
+
+	i = 0;
+	while (instruct[i].f != NULL && strcmp(instruct[i].opcode, str) != 0)
+	{
+		i++;
+	}
+
+	return (instruct[i].f);
+}
