b2a7fba25d2df404a478c47e1ef4f70b9352843d
Bright <bright.sablah@outlook.com>
2023-12-15 01:44:35 +0000

    Added _mul.c function

---
+++ bright/_mul.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+/**
+ * _mul - mul top of stack y second top stack
+ * @stack: pointer to lists for monty stack
+ * @line_number: number of line opcode occurs on
+ */
+void _mul(stack_t **stack, unsigned int line_number)
+{
+	int aux;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
+		free(var_global.buffer);
+		fclose(var_global.file);
+		free_dlistint(*stack);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		aux = (*stack)->n;
+		_pop(stack, line_number);
+		(*stack)->n *= aux;
+	}
+}
